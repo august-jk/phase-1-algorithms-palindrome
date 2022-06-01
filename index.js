3202a80f7307f71061beca1318e06064bb4b39fc
Aletha Wheeless <augustkama98@gmail.com>
2022-05-31 15:53:54 -1000

    commit tree 10bdc18042f9ec3c8fd0f6081f0685e3b7e4ec94

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if(reverse(word) === word) {
+    return true;
+  }
+  else{
+    return false;
+  }
 }
-
+function reverse(word) {
+  return word.split('').reverse().join('');
+}
 /* 
   Add your pseudocode here
-*/
+-iterate over string 
+check if string reads same forward and backward
+
+probably neeed .split to seperate string
+.split('')
+how to reverse string??
+.reverse()
+.join('')
+.join() returns seperated by comma
+with quotes returns correct
+
+  */
 
 /*
   Add written explanation of your solution here
